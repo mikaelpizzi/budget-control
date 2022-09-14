c615b68446494bcb83fbeae586140fccfb645d8a
Mikael <mikaelangelopizzi@gmail.com>
2022-09-14 18:45:31 -0400

    [ADD] Add hook to save entered budget

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Question from './components/Question';
 
+
 function App() {
   return (
     <div className='container'>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,21 +1,42 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+
 
 const Question = () => {
+    // Define state for budget
+    const [budget, saveBudget] = useState(0);
+    
+    // Read budget function
+    const defineBudget = e => {
+        saveBudget(parseInt(e.target.value, 10));
+    }
+    
+    // Submit event for define budget
+    const addBudget = e => {
+        e.preventDefault();
+
+        // Validate budget
+
+        // After validation
+    }
+
     return ( 
         <Fragment>
             <h2>Enter Your Budget</h2>
 
-            <form>
+            <form
+                onSubmit={addBudget}
+            >
                 <input
-                type='number'
-                className='u-full-width'
-                placeholder='Enter Your Budget'
+                    type='number'
+                    className='u-full-width'
+                    placeholder='Enter Your Budget'
+                    onChange={defineBudget}
                 />
 
                 <input
-                type='submit'
-                className='button-primary u-full-width'
-                value='Define Budget'
+                    type='submit'
+                    className='button-primary u-full-width'
+                    value='Define Budget'
                 />
             </form>
         </Fragment>
