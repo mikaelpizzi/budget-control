a0ee1ad93274ab59d96dc54df9e79825e88bf7c3
Mikael <mikaelangelopizzi@gmail.com>
2022-09-14 18:57:11 -0400

    [FEAT] Show message when budget is wrong

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,11 @@
 import React, { Fragment, useState } from 'react';
+import Error from './Error';
 
 
 const Question = () => {
     // Define state for budget
     const [budget, saveBudget] = useState(0);
+    const [error, saveError] = useState(false);
     
     // Read budget function
     const defineBudget = e => {
@@ -15,13 +17,20 @@ const Question = () => {
         e.preventDefault();
 
         // Validate budget
+        if (budget < 1 || isNaN(budget)) {
+            saveError(true);
+            return;
+        }
 
         // After validation
+        saveError(false);
     }
 
     return ( 
         <Fragment>
             <h2>Enter Your Budget</h2>
+
+            { error ? <Error message='Wrong Budget'/> : null }
 
             <form
                 onSubmit={addBudget}
