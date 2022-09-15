73daab48400445195a62a0863d8394aa54537dcf
Mikael <mikaelangelopizzi@gmail.com>
2022-09-15 19:46:14 -0400

    [ADD] Add states and events for validate and render a expense

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,24 @@
 import React, { useState } from 'react';
 
 const Form = (props) => {
+    const [name, setName] = useState('');
+    const [amount, setAmount] = useState(0);
+
+    // When user adds an expense
+    const addExpense = e => { 
+        // Validate
+
+        // Build the expense
+
+        // Pass the expense to principal component for list it
+
+        // Reset form 
+    }
 
     return ( 
-        <form>
+        <form
+        onSubmit={addExpense}
+        >
             <h2>Add your expenses here</h2>
 
             <div className='campo'>
@@ -12,15 +27,19 @@ const Form = (props) => {
                 type='text'
                 className='u-full-width'
                 placeholder='e.g. Transport'
+                value={name}
+                onChange={e => setName(e.target.value)}
                 />
             </div>
 
             <div className='campo'>
                 <label>Expense Amount</label>
                 <input 
-                type='text'
+                type='number'
                 className='u-full-width'
                 placeholder='e.g. 80'
+                value={amount}
+                onChange={e => setAmount(parseInt(e.target.value))}
                 />
             </div>
 
